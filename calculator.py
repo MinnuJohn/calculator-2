2cb8fedf44a350f2fa14310f8d0eb06dd3e6ac66
Minnu John <minnu.john@snhu.edu>
2023-05-18 18:29:33 -0500

    will quit if user input 'q'

--- calculator.py
+++ calculator.py
@@ -4,4 +4,23 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+# repeat forever:
+#     read input
+#     tokenize input
+#         if the first token is "q":
+#             quit
+#         else:
+#             (decide which math function to call based on first token)
+#             if the first token is 'pow':
+#                   call the power function with the other two tokens
+
+#             (...etc.)
+
+
+while True:
+    user_input = input("Enter your equation: ")
+    
+    token = user_input.split(" ")
+    if token[0] == 'q':
+        print("Quit")
+        break
